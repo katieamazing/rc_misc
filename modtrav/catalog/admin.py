85bd52b96b1dadc6ba2e75f47b7023fc59d29d8f
katieamazing <k.allen@live.com>
2017-07-03 16:02:01 -0400

    Last commit, cleanup

--- modtrav/catalog/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Tag, Location, Activity
-
-admin.site.register(Tag)
-admin.site.register(Location)
-admin.site.register(Activity)
--- xmastunes.md
+++
@@ -1,8 +0,0 @@
-to the tune of "O Christmas Tree"
-
-Binary Tree, Binary Tree
-Your nodes are bright and sparkly
-I traverse you recursively
-Binary Tree, Binary Tree
-
-hip hip array
